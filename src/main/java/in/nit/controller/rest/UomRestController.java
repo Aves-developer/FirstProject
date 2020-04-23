193056ae697cc5b4965bf4b9581ac5eac348f0eb
Avais <Avais@Avais>
2020-04-23 22:02:48 +0530

    rest controller code

---
+++ src/main/java/in/nit/controller/rest/UomRestController.java
@@ -0,0 +1,34 @@
+package in.nit.controller.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.nit.model.Uom;
+import in.nit.service.IUomService;
+
+@RestController
+@RequestMapping("/rest/uom")
+public class UomRestController {
+	@Autowired
+	private IUomService service;
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Uom>> fetchAllUoms(){
+	List<Uom>uoms=	service.getAllUom();
+	ResponseEntity<List<Uom>> resp=new ResponseEntity<List<Uom>>(uoms,HttpStatus.OK);
+		return resp;
+		
+	}
+	@GetMapping("/one")
+	public ResponseEntity<Uom> getOneUom(@RequestParam("id")Integer uomId){
+		Uom uom=service.getOneUom(uomId);
+		ResponseEntity<Uom> resp= new ResponseEntity<Uom>(uom,HttpStatus.OK);
+		return resp;
+	}
+}
