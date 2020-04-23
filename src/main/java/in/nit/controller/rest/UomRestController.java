3cdf8eced19df20975c1c771daf1439d6f415859
Avais <Avais@Avais>
2020-04-23 23:00:13 +0530

    Rest controller

--- src/main/java/in/nit/controller/rest/UomRestController.java
+++ src/main/java/in/nit/controller/rest/UomRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,5 +31,12 @@ public class UomRestController {
 		Uom uom=service.getOneUom(uomId);
 		ResponseEntity<Uom> resp= new ResponseEntity<Uom>(uom,HttpStatus.OK);
 		return resp;
+	}
+	@DeleteMapping("/remove")
+	public ResponseEntity<String> removeOneUom(@RequestParam("id")Integer uomId){
+		        service.deleteOneUOm(uomId);
+		        ResponseEntity<String> resp= new ResponseEntity<String>("One'"+uomId+"'is deleted",HttpStatus.OK);
+		return resp;
+		
 	}
 }
